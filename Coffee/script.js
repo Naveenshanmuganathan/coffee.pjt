62e46c96d3223f8833cca0915010f00c6b9f3060
Naveenshanmuganathan <158690550+Naveenshanmuganathan@users.noreply.github.com>
2024-06-12 19:19:17 +0530

    Add files via upload

---
+++ Coffee/script.js
@@ -0,0 +1,56 @@
+
+let innerMenu = document.getElementById(`inner-menu`);
+let nextBtn = document.getElementById("nextBtn");
+let prevBtn = document.getElementById('prevBtn');
+
+let add = 0;
+
+let nextAdd = -280;
+let prevAdd = 280;
+
+let windowScreenSize = window.innerWidth;
+console.log(windowScreenSize);
+
+if (windowScreenSize <= 430) {
+  windowScreenSize = -1280;
+  nextAdd = -320;
+  prevAdd = 320;
+}
+else if (windowScreenSize <= 1024) {
+  windowScreenSize = -840;
+  nextAdd = -280;
+  prevAdd = 280;
+}
+else {
+  windowScreenSize = -280;
+}
+
+nextBtn.addEventListener("click", () => {
+
+
+  if (add === 0) {
+    add = nextAdd;
+    console.log(add);
+    innerMenu.style.transform = `translateX(${add}px)`;
+  }
+  else if (add <= windowScreenSize) {
+    return;
+  }
+  else {
+    add = add - prevAdd;
+    console.log(add);
+    innerMenu.style.transform = `translateX(${add}px)`;
+
+  }
+});
+
+
+prevBtn.addEventListener("click", () => {
+  if (add === 0) {
+    return;
+  } else {
+
+    add = add + prevAdd;
+    innerMenu.style.transform = `translateX(${add}px)`;
+  }
+})
